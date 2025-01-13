dd8793de4b2c29805d94c84289310a7ffd047077
csahu-facilgo <98304410+csahu-facilgo@users.noreply.github.com>
2025-01-14 01:05:52 +0530

    Test-Case-2

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe StringCalculator do
     it "return 0 for an empty string" do
       expect(calculator.add("")).to eq(0)
     end
+
+    it "return 11 for an '11' string" do
+      expect(calculator.add("11")).to eq(11)
+    end
   end
 end
