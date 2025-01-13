d7fa1fd16225eb28de5d15538de53ccd5955a09c
csahu-facilgo <98304410+csahu-facilgo@users.noreply.github.com>
2025-01-14 01:07:37 +0530

    Test-Case-3

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe StringCalculator do
     it "return 11 for an '11' string" do
       expect(calculator.add("11")).to eq(11)
     end
+
+    it "return the sum of two numbers" do
+      expect(calculator.add("1,33")).to eq(34)
+    end
+
+    it "return the sum of unknown number of input" do
+      expect(calculator.add("1,2,3,3,5")).to eq(14)
+    end
   end
 end
