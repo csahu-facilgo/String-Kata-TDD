747922a3a6638db652c9711c0ad83be57ebe7964
csahu-facilgo <98304410+csahu-facilgo@users.noreply.github.com>
2025-01-14 01:22:30 +0530

    Test-Case-9

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,14 +6,14 @@ class StringCalculator
 
     if numbers.start_with?("//")
       delimiter, input = numbers[2..].split("\n", 2)
-      input.split(delimiter).reject(&:empty?).map(&:to_i).sum
+      num =input.split(delimiter).reject(&:empty?).map(&:to_i)
     else
       num = numbers.split(/,|\n/).reject(&:empty?).map(&:to_i)
-
-      negatives = num.select(&:negative?)
-
-      raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
-      num.sum
     end
+
+    negatives = num.select(&:negative?)
+
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+    num.sum
   end
 end
